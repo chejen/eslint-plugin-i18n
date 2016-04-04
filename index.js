aad68f44f72d97d7071a5f07dac76260463fc34a
Erwin Hom <erwin.hom@gmail.com>
2016-04-04 13:21:47 -0700

    added two i18n rules

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 module.exports = {
   rules: {
     'no-chinese-character': require('./lib/rules/no-chinese-character')
+    'no-non-ascii-character': require('./lib/rules/no-non-ascii-character')
+    'no-date-pattern': require('./lib/rules/no-date-pattern')
   },
   rulesConfig: {
     'no-chinese-character': 0
+    'no-non-ascii-character': 0
+    'no-date-pattern': 0
   }
 };
