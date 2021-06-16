f978b2203740417a48ea1de3248ca9ead2ba3f49
chejen <jkopre.qek@gmail.com>
2021-06-16 21:08:05 +0800

    [upgraded] husky and lint-staged

--- tests/lib/rules/no-korean-character.js
+++ tests/lib/rules/no-korean-character.js
@@ -62,23 +62,23 @@ ruleTester.run('no-korean-character', rule, {
     },
     {
       code: 'console.log(\'english\' + \'한국어\');',
-      errors: [{ message: 'Using Korean characters: \'한국어\'', type: 'Literal' }]
+      errors: [{ message: 'Using Korean characters: \'한국어\'', type: 'Literal' }],
     },
     {
       code: 'var str = \'문자열\'.substr(0, 1);',
-      errors: [{ message: 'Using Korean characters: \'문자열\'', type: 'Literal' }]
+      errors: [{ message: 'Using Korean characters: \'문자열\'', type: 'Literal' }],
     },
     {
       code: 'var obj = { \'key\': \'사물\' };',
-      errors: [{ message: 'Using Korean characters: \'사물\'', type: 'Literal' }]
+      errors: [{ message: 'Using Korean characters: \'사물\'', type: 'Literal' }],
     },
     {
       code: 'var func = function(v){return v;}; func(\'함수\');',
-      errors: [{ message: 'Using Korean characters: \'함수\'', type: 'Literal' }]
+      errors: [{ message: 'Using Korean characters: \'함수\'', type: 'Literal' }],
     },
     {
       code: 'var ary = ["배열"];',
-      errors: [{ message: 'Using Korean characters: "배열"', type: 'Literal' }]
+      errors: [{ message: 'Using Korean characters: "배열"', type: 'Literal' }],
     },
     {
       code: `
